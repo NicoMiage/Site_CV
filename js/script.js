048ce2b0e2727a49d3c116474ad09668d3f7d1bb
NicoMiage <nicolas.kircun@gmail.com>
2018-02-19 09:35:50 +0100

    Avancement développment

--- js/script.js
+++ js/script.js
@@ -18,3 +18,30 @@ $(function() {
     // On modifie la hauteur de toutes les cartes
     $('.card-pro').height(pro_height);
 });
+
+$(document).ready(function(){
+    $('.modal').modal();
+
+    $('#contact').click(function() {
+        var name = '&#110;&#105;&#99;&#111;&#108;&#97;&#115;&#46;&#107;&#105;&#114;&#99;&#117;&#110;'
+        var domain = '&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;'
+        var tel = '&#43;&#51;&#51;&#54;&#49;&#49;&#49;&#48;&#48;&#52;&#54;&#50;';
+        var mail = $('#mail');
+        var telButton = $('#tel');
+        mail.attr('href', 'mailto:' + name + '@' + domain);
+        mail.html(name + '@' + domain);
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            telButton.attr('href', 'tel:' + tel);
+        } else {
+            telButton.attr('style', 'cursor: context-menu');
+        }
+        telButton.html(tel);
+    });
+
+    var options = [
+        {selector: '#technos', offset: 0, callback: function() {
+            Materialize.showStaggeredList('#technos');
+        } },
+    ];
+    Materialize.scrollFire(options);
+});
